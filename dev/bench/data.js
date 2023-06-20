921256b13ba20a5966a02cfbd335191770ef95f4
github-action-benchmark <github@users.noreply.github.com>
2023-06-20 18:30:00 +0000

    add Rust Benchmark (cargo) benchmark result for f573170fad6f90db57bdc803fa0a847e5659d46a

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1687285800612,
+  "repoUrl": "https://github.com/fission-codes/rs-car-mirror",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "appcypher@outlook.com",
+            "name": "Stephen Akinyemi",
+            "username": "appcypher"
+          },
+          "committer": {
+            "email": "appcypher@outlook.com",
+            "name": "Stephen Akinyemi",
+            "username": "appcypher"
+          },
+          "distinct": true,
+          "id": "f573170fad6f90db57bdc803fa0a847e5659d46a",
+          "message": "Initial commit",
+          "timestamp": "2023-06-20T19:27:44+01:00",
+          "tree_id": "ce15b543739de6dc482d7e8420f88ef71bb53047",
+          "url": "https://github.com/fission-codes/rs-car-mirror/commit/f573170fad6f90db57bdc803fa0a847e5659d46a"
+        },
+        "date": 1687285799995,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "add",
+            "value": 2,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
