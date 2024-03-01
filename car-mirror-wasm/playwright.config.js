cd486534b4b087b578443a769300fda43514c706
Philipp Krüger <philipp.krueger1@gmail.com>
2024-03-01 16:48:54 +0100

    chore: Run tests for both HTTP and HTTPS

--- car-mirror-wasm/playwright.config.js
+++ car-mirror-wasm/playwright.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -26,6 +26,9 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto("/")`. */
     baseURL: "http://127.0.0.1:8081",
+
+    // We use fetch(https://) with self-signed certs on the axum server
+    ignoreHTTPSErrors: true,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
--- car-mirror-wasm/test/protocol.spec.js
+++ car-mirror-wasm/test/protocol.spec.js
@@ -22,71 +22,89 @@ test.beforeEach(async ({ page }) => {
   await page.waitForFunction(() => window.runCarMirrorPull != null);
 })
 
-test("car mirror pull", async ({ page }) => {
-  const { hasRootBlock, totalBlocks } = await page.evaluate(async () => {
-    const store = new MemoryBlockStore();
-    // This is the sample data that the server serves. 100MB of ChaCha8 data from the 0 seed encoded as UnixFS file
-    const cid = CID.parse("bafyb4ifjd76kkpos2uiv5mqifs4vi2xtywhf7pnu2pqtlg2vzmtmpyzdfa");
-    await runCarMirrorPull("http://localhost:3344/dag/pull", cid.toString(), store);
-    return ({
-      hasRootBlock: await store.hasBlock(cid.bytes),
-      totalBlocks: store.store.size
-    });
-  });
+test("car mirror pull http", testPull("http"));
+test("car mirror pull https", testPull("https"));
 
-  expect(hasRootBlock).toBeTruthy();
-  expect(totalBlocks).toBeGreaterThan(300);
-});
+test("car mirror incremental push http", testIncrementalPush("http"));
+test("car mirror incremental push https", testIncrementalPush("https"));
 
-test("car mirror push & incremental", async ({ page }) => {
-  const success = await page.evaluate(async () => {
-    const store = new MemoryBlockStore();
-    const wasmCid = await exampleFile(store, async (file) => {
-      const wasm = await (await fetch("./dist/web/car_mirror_wasm_bg.wasm")).arrayBuffer();
-      for (let i = 0; i < 5; i++) {
-        file.write(new Uint8Array(wasm));
-      }
-    });
-    await runCarMirrorPush("http://localhost:3344/dag/push", wasmCid.toString(), store);
-    return "success";
-  });
-  expect(success).toBe("success");
+test("car mirror both push then pull http", testPushThenPull("http"));
+test("car mirror both push then pull https", testPushThenPull("https"));
 
-  // and then push another time, this time more data, but sharing data with the previous push
-  const secondSuccess = await page.evaluate(async () => {
-    const store = new MemoryBlockStore();
-    const wasmCid = await exampleFile(store, async (file) => {
-      const wasm = await (await fetch("./dist/web/car_mirror_wasm_bg.wasm")).arrayBuffer();
-      for (let i = 0; i < 10; i++) {
-        file.write(new Uint8Array(wasm));
-      }
-    });
-    await runCarMirrorPush("http://localhost:3344/dag/push", wasmCid.toString(), store);
-    return "success";
-  });
-  expect(secondSuccess).toBe("success");
-});
 
-test("car mirror push then pull", async ({ page }) => {
-  const { hasRootBlock, totalBlocks } = await page.evaluate(async () => {
-    let store = new MemoryBlockStore();
-    const wasmCid = await exampleFile(store, async (file) => {
-      const wasm = await (await fetch("./dist/web/car_mirror_wasm_bg.wasm")).arrayBuffer();
-      for (let i = 0; i < 5; i++) {
-        file.write(new Uint8Array(wasm));
-      }
-    });
-    await runCarMirrorPush("http://localhost:3344/dag/push", wasmCid.toString(), store);
+function testPull(protocol) {
+  return async ({ page }) => {
+    const { hasRootBlock, totalBlocks } = await page.evaluate(async (protocol) => {
+      const store = new MemoryBlockStore();
+      // This is the sample data that the server serves. 100MB of ChaCha8 data from the 0 seed encoded as UnixFS file
+      const cid = CID.parse("bafyb4ifjd76kkpos2uiv5mqifs4vi2xtywhf7pnu2pqtlg2vzmtmpyzdfa");
+      await runCarMirrorPull(`${protocol}://localhost:3344/dag/pull`, cid.toString(), store);
+      return ({
+        hasRootBlock: await store.hasBlock(cid.bytes),
+        totalBlocks: store.store.size
+      });
+    }, protocol);
 
-    // Clear the store
-    store = new MemoryBlockStore();
-    await runCarMirrorPull("http://localhost:3344/dag/pull", wasmCid.toString(), store);
+    expect(hasRootBlock).toBeTruthy();
+    expect(totalBlocks).toBeGreaterThan(300);
+  }
+}
 
-    return ({
-      hasRootBlock: await store.hasBlock(wasmCid.bytes),
-      totalBlocks: store.store.size
-    });
-  });
-  expect(hasRootBlock).toBeTruthy();
-  expect(totalBlocks).toBeGreaterThan(10);
-})
+
+function testIncrementalPush(protocol) {
+  return async ({ page }) => {
+    const success = await page.evaluate(async (protocol) => {
+      const store = new MemoryBlockStore();
+      const wasmCid = await exampleFile(store, async (file) => {
+        const wasm = await (await fetch("./dist/web/car_mirror_wasm_bg.wasm")).arrayBuffer();
+        for (let i = 0; i < 5; i++) {
+          file.write(new Uint8Array(wasm));
+        }
+      });
+      await runCarMirrorPush(`${protocol}://localhost:3344/dag/push`, wasmCid.toString(), store);
+      return "success";
+    }, protocol);
+    expect(success).toBe("success");
+
+    // and then push another time, this time more data, but sharing data with the previous push
+    const secondSuccess = await page.evaluate(async (protocol) => {
+      const store = new MemoryBlockStore();
+      const wasmCid = await exampleFile(store, async (file) => {
+        const wasm = await (await fetch("./dist/web/car_mirror_wasm_bg.wasm")).arrayBuffer();
+        for (let i = 0; i < 10; i++) {
+          file.write(new Uint8Array(wasm));
+        }
+      });
+      await runCarMirrorPush(`${protocol}://localhost:3344/dag/push`, wasmCid.toString(), store);
+      return "success";
+    }, protocol);
+    expect(secondSuccess).toBe("success");
+  }
+}
+
+
+function testPushThenPull(protocol) {
+  return async ({ page }) => {
+    const { hasRootBlock, totalBlocks } = await page.evaluate(async (protocol) => {
+      let store = new MemoryBlockStore();
+      const wasmCid = await exampleFile(store, async (file) => {
+        const wasm = await (await fetch("./dist/web/car_mirror_wasm_bg.wasm")).arrayBuffer();
+        for (let i = 0; i < 5; i++) {
+          file.write(new Uint8Array(wasm));
+        }
+      });
+      await runCarMirrorPush(`${protocol}://localhost:3344/dag/push`, wasmCid.toString(), store);
+
+      // Clear the store
+      store = new MemoryBlockStore();
+      await runCarMirrorPull(`${protocol}://localhost:3344/dag/pull`, wasmCid.toString(), store);
+
+      return ({
+        hasRootBlock: await store.hasBlock(wasmCid.bytes),
+        totalBlocks: store.store.size
+      });
+    }, protocol);
+    expect(hasRootBlock).toBeTruthy();
+    expect(totalBlocks).toBeGreaterThan(10);
+  }
+}
